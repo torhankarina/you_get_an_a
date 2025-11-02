ba1fdf38c9acc8fdc70c626fd2ae84e7d87f4946
torhankarina <torhankarina@gmail.com>
2025-11-02 12:31:58 -0600

    implemented functions

--- src/gradebook.cpp
+++ src/gradebook.cpp
@@ -1,46 +1,47 @@
 #include "gradebook.hpp"
-#include <iostream>
 #include <sstream>
+#include <iomanip>
+#include <iostream>
 
-using namespace std;
-
-void Gradebook::add_student(const string& firstName, const string& lastName, const string& id) {
-   students.push_back({firstName, lastName, id});
+void Gradebook::add_student(const std::string &first, const std::string &last, const std::string &id) {
+   students.push_back({first, last, id});
 }
 
-void Gradebook::add_assignment(const string& name, int totalPoints) {
-   assignments.push_back({name, totalPoints});
+void Gradebook::add_assignment(const std::string &name, int total_points) {
+   assignments.push_back({name, total_points});
 }
 
-void Gradebook::enter_grade(const string& studentFullName, const string& assignmentName, int grade) {
-   for (const auto& s : students) {
-      string full = s.firstName + " " + s.lastName;
-      if (full == studentFullName) {
-         grades[s.studentID][assignmentName] = grade;
+void Gradebook::enter_grade(const std::string &student_name, const std::string &assignment_name, int grade) {
+   // Find student by "First Last"
+   for (auto &s : students) {
+      std::string full_name = s.first_name + " " + s.last_name;
+      if (full_name == student_name) {
+         grades[s.id][assignment_name] = grade;
          return;
       }
    }
-   cout << "Student not found: " << studentFullName << endl;
+   std::cout << "Student " << student_name << " not found!\n";
 }
 
-string Gradebook::report() const {
-   stringstream ss;
-   ss << "Last_Name,First_Name,Student_Id";
-   for (const auto& a : assignments)
-      ss << "," << a.name;
-   ss << "\n";
+std::string Gradebook::report() const {
+   std::ostringstream out;
+   out << "Last_Name,First_Name,Student_Id";
 
-   for (const auto& s : students) {
-      ss << s.lastName << "," << s.firstName << "," << s.studentID;
-      for (const auto& a : assignments) {
-         auto it = grades.find(s.studentID);
-         if (it != grades.end() && it->second.find(a.name) != it->second.end()) {
-            ss << "," << it->second.at(a.name);
-         } else {
-            ss << ",none";
-         }
+   for (auto &a : assignments)
+      out << "," << a.name;
+   out << "\n";
+
+   for (auto &s : students) {
+      out << s.last_name << "," << s.first_name << "," << s.id;
+      for (auto &a : assignments) {
+         auto student_it = grades.find(s.id);
+         if (student_it != grades.end() && student_it->second.count(a.name))
+            out << "," << student_it->second.at(a.name);
+         else
+            out << ",none";
       }
-      ss << "\n";
+      out << "\n";
    }
-   return ss.str();
+
+   return out.str();
 }
--- src/gradebook.hpp
+++ src/gradebook.hpp
@@ -3,27 +3,27 @@
 #include <vector>
 #include <map>
 
-using namespace std;
-
-struct Student {
-    string firstName;
-    string lastName;
-    string studentID;
-};
-
-struct Assignment {
-    string name;
-    int totalPoints;
-};
-
 class Gradebook {
 private:
-    vector<Student> students;
-    vector<Assignment> assignments;
-    map<string, map<string, int>> grades;
+    struct Student {
+        std::string first_name;
+        std::string last_name;
+        std::string id;
+    };
+
+    struct Assignment {
+        std::string name;
+        int total_points;
+    };
+
+    std::vector<Student> students;
+    std::vector<Assignment> assignments;
+    // grades[student_id][assignment_name] = grade
+    std::map<std::string, std::map<std::string, int>> grades;
+
 public:
-    void add_student(const string& firstName, const string& lastName, const string& id);
-    void add_assignment(const string& name, int totalPoints);
-    void enter_grade(const string& studentFullName, const string& assignmentName, int grade);
-    string report() const;
+    void add_student(const std::string &first, const std::string &last, const std::string &id);
+    void add_assignment(const std::string &name, int total_points);
+    void enter_grade(const std::string &student_name, const std::string &assignment_name, int grade);
+    std::string report() const;
 };
